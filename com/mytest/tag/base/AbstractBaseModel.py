2d2579255fe6eb5d0f25134d1f6b351db147c1ad
ellicoo <ellicoo@outlook.com>
2024-07-22 14:36:20 +0800

    fix abstractbasemodel

--- com/mytest/tag/base/AbstractBaseModel.py
+++ com/mytest/tag/base/AbstractBaseModel.py
@@ -20,8 +20,6 @@ os.environ['SPARK_HOME'] = '/export/server/spark'
 os.environ['PYSPARK_PYTHON'] = '/root/anaconda3/envs/pyspark_env/bin/python'
 os.environ['PYSPARK_DRIVER_PYTHON'] = '/root/anaconda3/envs/pyspark_env/bin/python'
 
-
-
 """
 #################步骤一：把公共方法抽取到父类中#################
 #0.初始化Spark环境
@@ -48,7 +46,20 @@ os.environ['PYSPARK_DRIVER_PYTHON'] = '/root/anaconda3/envs/pyspark_env/bin/pyth
 
 
 """
+
+
 # 自定义的函数，用来实现标签的合并
+# merge_tags(new_df['tagsId'], old_df['tagsId']）
+# 尽管函数merge_tags传递进去的参数是new_df['tagsId'], old_df['tagsId']，都是dataframe,但是由于merge_tags是个udf函数，实际上是传递进去的是
+# new_df['tagsId'], old_df['tagsId']这两个df的一行，是一行一行的数据作为udf的参数，而不是整个dataframe作为参数
+# 问题，new_df['tagsId'], old_df['tagsId']这两个df需要行数相等且userId逐行对应吗？
+# 需要，(注意，每次都是全量打标签)所以需要先left join，通过new_df['userId'] == old_df['userId']来实现本次udf行操作的两个df，都是操作同一个userId的tagsId
+# 1。在本次udf的行操作中，如果这个userId对应的新标签tagsId为None,即本行的userId没有中到任何rule条件不打标签，则跳出本次udf的行迭代，直接将旧的的标签作为新的合并后的标签，进入下一行的udf操作。
+# 2。在本次udf的行操作中，如果这个userId对应的新标签tagsId不为None，说明本行的userId打了新标签，而对应的old_df没打过标签，则直接将这个新标签作为合并后的标签
+# 3。在本次udf的行操作中，如果这个userId对应的新标签tagsId不为None，说明本行的userId打了新标签，但对应的old_df打过标签，此时需要进行标签合并操作
+
+# 总结，udf逐行操作，需要行对应，否则无法操作
+# new_df, old_df都是df的row行，而不是整个df
 @F.udf(returnType=StringType())
 def merge_tags(new_df, old_df, fiveTagIDStr):
     # 1.如果new_df为空，返回old_df数据
@@ -70,7 +81,7 @@ def merge_tags(new_df, old_df, fiveTagIDStr):
     # 比如访问周期，就会出现，即是0～7这个访问周期，又是7～14这个访问周期，而应该只保留最新的7～14这个tag周期。
 
     five_tag_id_list = fiveTagIDStr.split(",")
-    # 通过当前这个4级标签种类(因为4级标签种类很多)下的所有5级标签，将原来的已经打的这个4级标签种类下的所有旧的5级标签全部剔除，old_df_list只保留其他4级标签种类的所有5级标签
+    # 通过当前这个4级标签种类(因为4级标签种类很多)下的所有5级标签，将原来的已经打的这个4级标签种类下的所有旧的5级标签全部剔除，old_df_list只保留其他4级标签种类的其他类的所有5级标签
     # 将以前的同类标签拔除，保留其他类别标签
     for tag in five_tag_id_list:
         if tag in old_df_list:
@@ -102,6 +113,7 @@ class AbstractBaseModel:
 
     # 1.根据4级标签ID，读取MySQL整个标签体系数据，-- 这是一张标签体系表，将mysql的数据读成spark的dataframe表
     # 整个标签体系是细粒度规则和粗粒度规则都用一个字段存储，使用子母类id与pid进行树的数据结构的封装
+    # spark环境存储在对象成员变量中，使用self.spark才可以访问
     def read_mysql_data(self):
         input_df = self.spark.read.jdbc(url='jdbc:mysql://up01:3306/tfec_tags',
                                         table='tbl_basic_tag',
@@ -126,6 +138,7 @@ class AbstractBaseModel:
     #  参数说明：
     # esMeta信息--是包装出的新类型的对象--是因为预判4级标签规则里面指定的数据源的储存位置信息，切割成需要的几个参数信息，
     #            用对象存储起来--方便复用，比如read_old_df_from_es和write_result_df_to_es就再需要调用
+    # spark环境存储在对象成员变量中，使用self.spark才可以访问
     def read_es_df_by_esMeta(self, esMeta):
         es_df = self.spark.read \
             .format('es') \
@@ -180,69 +193,69 @@ class AbstractBaseModel:
     # 目标工作： 给这几个表的关键id打标签
 
 
-def write_result_df_to_es(self, result_df, esMeta):
-    result_df.write \
-        .format("es") \
-        .option("es.resource", "tags_result") \
-        .option("es.nodes", esMeta.esNodes) \
-        .option("es.mapping.id", "userId") \
-        .mode("append") \
-        .save()
+    def write_result_df_to_es(self, result_df, esMeta):
+        result_df.write \
+            .format("es") \
+            .option("es.resource", "tags_result") \
+            .option("es.nodes", esMeta.esNodes) \
+            .option("es.mapping.id", "userId") \
+            .mode("append") \
+            .save()
 
-    # 9.销毁Spark环境，释放资源，一样的
-
-
-def close(self):
-    self.spark.stop()
+        # 9.销毁Spark环境，释放资源，一样的
 
 
-"""
-自定义的execute方法，用于把上述的方法串起来执行
-"""
+    def close(self):
+        self.spark.stop()
 
 
-def execute(self):
-    # # 0.初始化Spark环境
-    self.__init__(self.fourTagId)
-    # 1.根据4级标签ID，读取MySQL标签体系数据--读出来的结果是一张表
-    input_df = self.read_mysql_data()
-    # 2.过滤4级标签的数据，将四级标签的rule转换为esMeta对象，id不同，逻辑相同
-    esMeta = self.input_df_to_esMeta(input_df)
-    # 3.根据esMeta对象从ES中读取相应的业务数据--读出来的结果是一张表--一张待匹配或者待统计或者待挖掘的业务数据的表
-    es_df = self.read_es_df_by_esMeta(esMeta)
-    # 4.根据4级标签ID，读取5级标签的数据
-    # 刚刚我们只要input(来源与mysql)表中的rule字段来转换成meta对象去es中找待操作的数据表
-    # 现在我们还需要input表中的其他字段值，level字段为5的值，这个值有很多，但我只要刚刚指定的rule字段下的5级值，取出的是规则值
-    # 把规则值和刚刚找到的es表的数据进行操作(匹配、统计、挖掘)
-    five_df = self.read_five_df_by_fourTagId(input_df)
-    # 5.通过ES中的业务数据与MySQL的5级标签进行打标签，完全不一样
-    new_df = self.compute(es_df, five_df)
-    try:
-        # 6.从ES中读取历史用户标签数据
-        old_df = self.read_old_df_from_es(esMeta)
-        # 标签更新
-        """
-        需要传入改4级标签下的所有5级标签的ID号。不能直接给five_df（ID、rule）
-        （1）通过five_df获取所有ID
-        （2）传入所有ID（List）List不能直接传入到自定义函数中，自定义需要只能传入2种类型（Column、Str）
-        （3）把list转换为字符串，使用固定的分割符号拼接
-            ",".join(List)
-        """
-        fiveTagIDList = five_df.rdd.map(lambda x: x.id).collect()
+    """
+    自定义的execute方法，用于把上述的方法串起来执行
+    """
 
-        # 7.将老的用户画像标签与新的标签进行合并，得到最终标签
 
-        """
-        ",".join(str(id) for id in fiveTagIDList) 首先将 fiveTagIDList 中的所有 ID 转换为字符串，并用逗号连接成一个字符串。
-        F.lit() 然后将这个生成的字符串转换为 Spark 列对象，这个列对象的值在 DataFrame 的每一行中都是相同的
-        """
-        result_df = self.merge_old_df_and_new_df(new_df, old_df, F.lit(",".join(str(id) for id in fiveTagIDList)))
-    except:
-        # 如果出了问题，说明这个任务是第一次运行，没有历史标签库
-        # 可以使用new_df当做标签结果数据，避免合并出问题
-        result_df = new_df
-        print("--------------首次执行，跳过合并---------------")
-    # 8.将最终的结果写入ES中
-    self.write_result_df_to_es(result_df, esMeta)
-    # 9.销毁Spark环境，释放资源
-    self.close()
+    def execute(self):
+        # # 0.初始化Spark环境--本类的魔法函数已经做了spark的初始化spark对象的操作
+        self.__init__(self.fourTagId)
+        # 1.根据4级标签ID，读取MySQL标签体系数据--读出来的结果是一张表
+        input_df = self.read_mysql_data()
+        # 2.过滤4级标签的数据，将四级标签的rule转换为esMeta对象，id不同，逻辑相同
+        esMeta = self.input_df_to_esMeta(input_df)
+        # 3.根据esMeta对象从ES中读取相应的业务数据--读出来的结果是一张表--一张待匹配或者待统计或者待挖掘的业务数据的表
+        es_df = self.read_es_df_by_esMeta(esMeta)
+        # 4.根据4级标签ID，读取5级标签的数据
+        # 刚刚我们只要input(来源与mysql)表中的rule字段来转换成meta对象去es中找待操作的数据表
+        # 现在我们还需要input表中的其他字段值，level字段为5的值，这个值有很多，但我只要刚刚指定的rule字段下的5级值，取出的是规则值
+        # 把规则值和刚刚找到的es表的数据进行操作(匹配、统计、挖掘)
+        five_df = self.read_five_df_by_fourTagId(input_df)
+        # 5.通过ES中的业务数据与MySQL的5级标签进行打标签，完全不一样
+        new_df = self.compute(es_df, five_df)
+        try:
+            # 6.从ES中读取历史用户标签数据
+            old_df = self.read_old_df_from_es(esMeta)
+            # 标签更新
+            """
+            需要传入改4级标签下的所有5级标签的ID号。不能直接给five_df（ID、rule）
+            （1）通过five_df获取所有ID
+            （2）传入所有ID（List）List不能直接传入到自定义函数中，自定义需要只能传入2种类型（Column、Str）
+            （3）把list转换为字符串，使用固定的分割符号拼接
+                ",".join(List)
+            """
+            fiveTagIDList = five_df.rdd.map(lambda x: x.id).collect()
+
+            # 7.将老的用户画像标签与新的标签进行合并，得到最终标签
+
+            """
+            ",".join(str(id) for id in fiveTagIDList) 首先将 fiveTagIDList 中的所有 ID 转换为字符串，并用逗号连接成一个字符串。
+            F.lit() 然后将这个生成的字符串转换为 Spark 列对象，这个列对象的值在 DataFrame 的每一行中都是相同的
+            """
+            result_df = self.merge_old_df_and_new_df(new_df, old_df, F.lit(",".join(str(id) for id in fiveTagIDList)))
+        except:
+            # 如果出了问题，说明这个任务是第一次运行，没有历史标签库
+            # 可以使用new_df当做标签结果数据，避免合并出问题
+            result_df = new_df
+            print("--------------首次执行，跳过合并---------------")
+        # 8.将最终的结果写入ES中
+        self.write_result_df_to_es(result_df, esMeta)
+        # 9.销毁Spark环境，释放资源
+        self.close()
